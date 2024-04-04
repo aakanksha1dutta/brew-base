a96e5bb66068e86ebd5bced0a473ce485b3b0ba7
aakanksha1dutta <aakanksha1dutta@gmail.com>
2024-04-04 19:53:22 -0400

    made search_orders.html

---
+++ includes/html_php/menu.php
@@ -0,0 +1,33 @@
+<?php
+
+include_once 'connect_table_worker.php';
+
+$query= "SELECT * FROM MENU";
+$result = mysqli_query($conn, $query);
+$query = array();
+while($query[] = mysqli_fetch_assoc($result));
+array_pop($query);
+
+// Output a dynamic table of the results with column headings.
+echo "<h1> Menu Table </h1>";
+echo '<table border="1">';
+echo '<tr>';
+foreach($query[0] as $key => $value) {
+    echo '<td>';
+    echo $key;
+    echo '</td>';
+}
+echo '</tr>';
+foreach($query as $row) {
+    echo '<tr>';
+    foreach($row as $column) {
+        echo '<td>';
+        echo $column;
+        echo '</td>';
+    }
+    echo '</tr>';
+}
+echo '</table>';
+
+mysqli_close($mysqli);
+?>
